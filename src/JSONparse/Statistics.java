a38a94a16ce5b32b80a2fbcabb323eee82a77110
Ivan <41164283+IvaninoPanevino@users.noreply.github.com>
2019-06-24 00:28:11 +0200

    Update Statistics.java

--- src/JSONparse/Statistics.java
+++ src/JSONparse/Statistics.java
@@ -6,35 +6,38 @@ import java.util.List;
 
 public class Statistics extends Hotel{
 
-	private ArrayList<Hotel> hotel;
 	private int size;
 	public Statistics() {	
 	}
 
 	public void Statistics(ArrayList<Hotel> tabHotel) {
+		System.out.println(size);
 		int room[]= new int[size];
 		int bed[]= new int[size];
 		int toilet[]= new int[size];
 		String structureType[]= new String[size];
+		String businessType[]= new String[size];
+		String scia_aut[]= new String[size];
 		int i=0;
-		//hotel = tabHotel;
-		for (Hotel item: tabHotel)
+		for (Hotel item: tabHotel) //taking all the datas for tabHotel
 		{
 			room[i]=item.getRoom();
 			bed[i]=item.getBed();
 			toilet[i]=item.getToilet();
 			structureType[i]=item.getStructure();
+			businessType[i]=item.getBusiness();
+			scia_aut[i]=item.getSiaAut();
 			i++;
 		}
 		
-		this.Print(room, bed, toilet, structureType);
+		this.Print(room, bed, toilet,structureType, businessType,scia_aut); //Print the stats
 	}
 	
 	protected void setSize(int sizeTab) {
 		this.size=sizeTab;
 	}
 	
-	private void Print(int room[], int bed[], int toilet[], String structureType[]) {
+	private void Print(int room[], int bed[], int toilet[], String structureType[], String businessType[], String scia_aut[]) {
 		System.out.printf("%n%n:::::::::::::STATISTICS:::::::::::::%n%n");
 		
 		System.out.printf("%n---Rooms---%n");
@@ -50,7 +53,16 @@ public class Statistics extends Hotel{
 		System.out.printf("%n---Toilets---%n");
 		System.out.printf("Toilets average in hotels : %d%n", Average(toilet));
 		System.out.printf("Max toilets : %d%n", Max(toilet));
-		System.out.printf("Min toilets : %d %n%n", Min(toilet));
+		System.out.printf("Min toilets : %d%n%n", Min(toilet));
+
+		System.out.printf("%n---Structure--%n");
+		Count(structureType);
+		
+		System.out.printf("%n---Business--%n");
+		Count(businessType);
+		
+		System.out.printf("%n---Scia/Auth--%n");
+		Count(scia_aut);
 	}
 	
 	private int Average(int tab[]) {
@@ -78,5 +90,45 @@ public class Statistics extends Hotel{
 				min=tab[i];
 		}
 		return min;
+	}
+	
+	private void Count(String tab[]) {
+		String[] cat= new String[size];
+		int[] number= new int[size];
+		int flag=0, j=0;
+		
+		for(int i=0;i<size;i++)
+		{
+			cat[i]="";
+			number[i]=0;
+		} 
+
+		int nCat=1;
+		cat[0]=tab[0];
+		
+		   for (int i=0;i<size;i++) {
+		       while ( j<nCat) // search an existing category in the array
+		       {
+		           if (tab[i].equals(cat[j]))
+		           {
+		               number[j]++;
+		               flag=1;
+
+		           }
+		           j++;
+		       }
+		       if (flag==0) //if there is no existing category : create a new one
+		       {
+		           cat[j]=tab[i];
+		           number[j]++;
+		           nCat++;
+		       }
+		       j=0;
+		       flag=0;
+		   }
+		for(int i=0;i<nCat;i++) // print the number of each category
+		{
+			System.out.printf("Categorie : %s counted %d times%n", cat[i], number[i]);
+		} 
 	}
 }
